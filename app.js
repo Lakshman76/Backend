6bc510c1df46364b6e6dec8eba60169d99740799
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-18 18:47:41 +0530

    database configuration done

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 
+const dbConnect = require('./config/databaseConfig');
+
 const app = express();
+
+dbConnect();
 
 module.exports = app;
---
+++ config/databaseConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/my_database";
+
+const dbConnect = ()=>{
+    mongoose
+        .connect(MONGODB_URL)
+        .then((conn)=>{
+            console.log(`Connected to ${conn.connection.host}`);
+        })
+        .catch((err)=>{
+            console.log(err.message);
+        })
+}
+
+module.exports = dbConnect;
